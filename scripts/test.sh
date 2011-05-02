5d196874bd686fa9260e44f874614a9fc7401402
Dav Glass <davglass@gmail.com>
2011-05-02 14:03:15 -0500

    Updated test runner for npm 1.0 support

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-yuitest=`which yuitest`
-
-if [ ! -f "$yuitest" ]; then
-    echo "YUITest CLI is required.. (npm install yuitest)"; exit 1;
+if [ -d "./tests/node_modules" ]; then
+    echo "Found ./tests/node_modules, removing it"
+    rm -rRf ./tests/node_modules
+    wait
 fi
+
+if [ ! -d "./build/full" ]; then
+    echo "No current build found, building"
+    make full
+    wait
+fi
+
+cd tests;
+echo "Installing latest build locally for testing"
+npm i ../build/full --loglevel silent
+echo "Installing yuitest locally"
+npm i yuitest --loglevel silent
+echo "Installing gallery"
+npm i yui3-gallery@2010.09.22 --loglevel silent
+echo "Installing 2in3"
+npm i yui3-2in3 --loglevel silent
+wait
+cd ../
 
 #Checking Dependencies
 ./tests/deps.js
@@ -12,4 +30,4 @@ if test $? -gt 0; then
     echo "Test dependencies failed.."; exit 1;
 fi
 
-$yuitest ./tests/*.js
+./tests/node_modules/.bin/yuitest ./tests/*.js
