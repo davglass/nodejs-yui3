5bac6ba44ac9681de2e1fc33f145af7024d31f6c
Dav Glass <davglass@gmail.com>
2011-05-02 11:35:28 -0500

    Tests currently fail because of the tests dependencies on global npm modules that do not exist any more

--- tests/deps.js
+++ tests/deps.js
@@ -1,26 +1,20 @@
 #!/usr/bin/env node
 
 try {
-    require('yui3-core@3.2.0');
+    require('yui3');
 } catch (e) {
-    console.log('yui3-core@3.2.0 needs to be installed');
-    process.exit(1);
-}
-try {
-    require('yui3-core@3.3.0');
-} catch (e) {
-    console.log('yui3-core@3.3.0 needs to be installed');
-    process.exit(1);
-}
-try {
-    require('yui3-gallery@2010.09.22');
-} catch (e) {
-    console.log('yui3-gallery@2010.09.22 needs to be installed');
+    console.log('yui3 needs to be installed');
     process.exit(1);
 }
 try {
     require('yui3-2in3');
 } catch (e) {
     console.log('yui3-2in3 needs to be installed');
+    process.exit(1);
+}
+try {
+    require('yuitest');
+} catch (e) {
+    console.log('yuitest needs to be installed');
     process.exit(1);
 }
